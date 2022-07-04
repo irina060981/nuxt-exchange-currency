06079d5a348e3bb2c15a507a1d2f5a1295d41c09
Irina <irina060981@yandex.ru>
2022-07-04 23:11:43 +1000

    remove unused css

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,11 +20,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=PT+Serif&family=Work+Sans:wght@400;700&display=swap'
-      }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
 
